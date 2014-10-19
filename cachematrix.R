7240096a02cd460b1cb68644132aefe201451261
uit_12345 <uit_12345@yahoo.com>
2014-10-19 02:28:13 -0500

    My modifications
    Inverse Matrix calculation  based on function calling based on whether
    inverse already exists or not.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## The set function allows you to modify the original matrix and resets the inverse to null.
+## The get function allows you to retrieve the original matrix.
+## The setinverse function sets the new matrix inverse.
+## The getinverse function retrieves the inverse
+
+
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()){
+  
+  m <- NULL
+  
+  
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+    
+  }
+  
+  get <- function() {
+    x
+  }
+  setinverse <- function(inverse){ m <<- inverse }
+  
+  getinverse <- function() {
+    m
+  }
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
+  
 }
 
 
 ## Write a short comment describing this function
+## The function checks whether the inverse retrieved is null or not, if null it computes the new
+##inverse my stores it using the setinverse function. If not null then it just retieves the 
+##previously stored inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  
+  m <- solve(data)
+  x$setinverse(m)
+  m
 }
